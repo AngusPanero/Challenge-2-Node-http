8892174a036b138de19c7123d09367e9a61149ea
Agustin Panero <agustinpanero@MacBook-Pro-de-Agustin.local>
2024-12-23 09:36:56 +0100

    DataFetch Resuelta

--- server.mjs
+++ server.mjs
@@ -1,9 +1,23 @@
 import { pokemonFetch } from './data.mjs';
-
 import http from 'node:http';
 
-const servidor = http.createServer((req, res) =>
-    res.end(`Request Recived`));
+let data = null;
+
+
+const servidor = http.createServer((req, res) => {
+    if (req.url === '/data' && data) {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(data));
+    } else {
+        res.end(`Error en la Solicitud`);
+    }
+});
+
+
+(async () => {
+    const data = await pokemonFetch()
+    console.log("Esta es la Data Importada", JSON.stringify(data))
+})();
 
 servidor.listen(0, () => {
     console.log(`Respuesta Exitosa desde el servidor ${servidor.address().port}`);
